3172f79b89b331c5e0253b8d05155b32eba18079
Professor_V2 <78028717+ProfessorV2@users.noreply.github.com>
2022-10-25 19:45:53 +0100

    Update sh_init.lua

--- gamemode/modules/ui/sh_init.lua
+++ gamemode/modules/ui/sh_init.lua
@@ -1,10 +1,19 @@
 local MODULE = MODULE
+
 MODULE.name = "Professors UI"
 MODULE.author = "professor"
 MODULE.description = "This is the main UI for the server"
+
 GNIL.Ui = GNIL.Ui or {}
 
-local professorsWeirdLoadOrder = {"libs/cl_gnil_ui.lua", "classes/cl_button.lua", "classes/cl_cats.lua", "classes/cl_shadows.lua", "classes/cl_scroll.lua", "classes/cl_frame.lua"}
+local professorsWeirdLoadOrder = {
+    "libs/cl_gnil_ui.lua",
+    "classes/cl_button.lua",
+    "classes/cl_cats.lua",
+    "classes/cl_shadows.lua",
+    "classes/cl_scroll.lua",
+    "classes/cl_frame.lua"
+}
 
 for _, v in ipairs(professorsWeirdLoadOrder) do
     MODULE:Include(v)
