f102bbfe956f23fe64348bbff82bf184443678d0
MorgVerd <morgverd@gmail.com>
2022-11-03 03:33:14 +0000

    Add IsDeveloper & restructure

--- gamemode/core/classes/sv_player.lua
+++ gamemode/core/classes/sv_player.lua
@@ -1,10 +1,15 @@
-
 local PlayerMeta = FindMetaTable("Player")
 
 -- Allow for logging directly to a player instance
-function PlayerMeta:log(log, logtype)
-    GNIL.Logging.LogToPlayer(self, log, logtype)
-end
-
--- Uppercase alias for dumbasses
+function PlayerMeta:log(log, logtype) GNIL.Logging.LogToPlayer(self, log, logtype) end
 PlayerMeta.Log = PlayerMeta.log
+
+-- Add IsDeveloper check similar to IsAdmin. Uses the
+-- steamid64 information from the server credits table.
+local DeveloperSteamIDs = {}
+for _, v in ipairs(GNIL._CREDITS) do
+    DeveloperSteamIDs[v["id64"]] = true
+end
+function PlayerMeta:IsDeveloper()
+    return DeveloperSteamIDs[self:SteamID64()] == true
+end
