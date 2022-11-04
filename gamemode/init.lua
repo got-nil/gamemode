9dbfb135b3c6953ffc53fda58cb228916d2c3a80
MorgVerd <morgverd@gmail.com>
2022-11-04 03:17:46 +0000

    refactor: Remove PlayerNetLoad from gm init

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -9,13 +9,3 @@ GM.DarkRP = BaseClass
 -- this is the core gamemode entrypoint/setup.
 AddCSLuaFile("shared.lua")
 include("shared.lua")
-
--- This will eventually be moved to the net lib.
-hook.Add("PlayerInitialSpawn", "gnil_setup_load", function(ply)
-	hook.Add("SetupMove", ply, function(self, pl, _, cmd)
-		if self == pl and not cmd:IsForced() then
-			hook.Run("PlayerNetLoad", self)
-			hook.Remove("SetupMove", self)
-		end
-	end)
-end)
