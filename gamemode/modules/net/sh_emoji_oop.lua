db97ead19e6cd85b6a86be155dc43be2b88200e2
MorgVerd <morgverd@gmail.com>
2022-11-05 03:22:56 +0000

    refactor: Make the EmojiMessage with emojis

--- gamemode/modules/net/sh_emoji_oop.lua
+++ gamemode/modules/net/sh_emoji_oop.lua
@@ -1,21 +1,20 @@
 
-local EmojiNetworkMessage = GNIL.Thirdparty.middleclass("EmojiNetworkMessage", GNIL.Net.NetworkMessage)
 
-function EmojiNetworkMessage:Initialize(...)
-    GNIL.Net.NetworkMessage.Initialize(self, ...)
-end
+local 🚢 = GNIL.Thirdparty.middleclass("🚢", GNIL.Net.NetworkMessage)
+local 🏠 = GNIL.Net.NetworkMessage
 
-function EmojiNetworkMessage:✏️📐(...)  return GNIL.Net.NetworkMessage.WriteAngle(self, ...)  end
-function EmojiNetworkMessage:✏️🎨(...)  return GNIL.Net.NetworkMessage.WriteColor(self, ...)  end
-function EmojiNetworkMessage:✏️🤸(...) return GNIL.Net.NetworkMessage.WriteEntity(self, ...) end
-function EmojiNetworkMessage:✏️📍(...) return GNIL.Net.NetworkMessage.WriteVector(self, ...) end
-function EmojiNetworkMessage:✏️🔢(...)    return GNIL.Net.NetworkMessage.WriteInt(self, ...)    end
-function EmojiNetworkMessage:✏️📄(...) return GNIL.Net.NetworkMessage.WriteString(self, ...) end
+function 🚢:Initialize(...) 🏠.Initialize(self, ...) end
+function 🚢:✏️📐(...)  return 🏠.WriteAngle(self, ...)  end
+function 🚢:✏️🎨(...)  return 🏠.WriteColor(self, ...)  end
+function 🚢:✏️🤸(...) return 🏠.WriteEntity(self, ...) end
+function 🚢:✏️📍(...) return 🏠.WriteVector(self, ...) end
+function 🚢:✏️🔢(...) return 🏠.WriteInt(self, ...)    end
+function 🚢:✏️📄(...) return 🏠.WriteString(self, ...) end
 
-function EmojiNetworkMessage:✉️(ply) return GNIL.Net.NetworkMessage.Send(self, ply) end
-function EmojiNetworkMessage:📢() return GNIL.Net.NetworkMessage.Broadcast(self) end
-function EmojiNetworkMessage:✉️🖥️() return GNIL.Net.NetworkMessage.SendToServer(self) end
+function 🚢:✉️(ply) return 🏠.Send(self, ply) end
+function 🚢:📢() return 🏠.Broadcast(self) end
+function 🚢:✉️🖥️() return 🏠.SendToServer(self) end
 
-function GNIL.Net.🛠️(...) return EmojiNetworkMessage:New(...) end
-function 🛠️(...) return EmojiNetworkMessage:New(...) end
+function GNIL.Net.🛠️(...) return 🚢:New(...) end
+function 🛠️(...) return 🚢:New(...) end
 🌐 = GNIL.Net
