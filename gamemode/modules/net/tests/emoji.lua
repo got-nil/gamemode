95e7b7cde03a340f62e75e1d8321ef1a10fc3018
MorgVerd <morgverd@gmail.com>
2022-11-07 00:56:35 +0000

    test: Remove net tests directory

--- gamemode/modules/net/tests/emoji.lua
+++
@@ -1,9 +0,0 @@
-
-local 📩 = 🌐.🛠️("test")
-
-📩:✏️📄("This is the future of GMod Networking") -- string
-📩:✏️🎨(me:GetColor()) -- color
-📩:✏️📐(me:EyeAngles()) -- angle
-📩:✏️📍(me:GetPos()) -- vector
-
-📩:✉️🖥️() -- send the message to the server
