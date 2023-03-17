092aeadb5575e53c5b781a03b00193dc0f73e0d4
MorgVerd <morgverd@gmail.com>
2023-03-17 23:45:25 +0000

    feat: Update whitelist concommands

--- gamemode/modules/antiskid/sv_command.lua
+++ gamemode/modules/antiskid/sv_command.lua
@@ -1,5 +1,6 @@
 
-local whitelist_set_callback = function(ply, args)
+-- Set a specific AntiSkid check whitelist.
+local whitelist_set_check_callback = function(ply, args)
     if #args == 0 then
         ply:log("Missing required argument <check> (SkidCheck check name)", "warning")
         return
@@ -29,4 +30,25 @@ local whitelist_set_callback = function(ply, args)
     end)
 end
 
-GNIL.Commands.Add("antiskid-whitelist-set", whitelist_set_callback, {GNIL_CMD_ARGUMENT_STRING}, GNIL_CMD_ACCESS_DEVELOPER, true)
+-- Set all AntiSkid checks that use whitelists.
+local whitelist_set_all_callback = function(ply)
+    for _, v in pairs(GNIL.AntiSkid["_checks"]) do
+        if not v.whitelist then return end
+        
+        ply:log("Starting whitelist SkidCheck '" .. v.name .. "'", "info")
+        v:Start(ply, function(success, out)
+            if not success then
+                ply:log("Failed to set check whitelist data with error: " .. out, "error")
+                return
+            end
+
+            v.whitelist:Write(out)
+            ply:log("Successfully updated AntiSkid check '" .. check.name .. "' whitelist", "success")
+        end)
+    end
+    ply:log("Finished setting all SkidCheck whitelists based on player " .. ply:ToString(), "success")
+end
+
+-- Register commands.
+GNIL.Commands.Add("antiskid-whitelist-set-all", whitelist_set_all_callback, {GNIL_CMD_ARGUMENT_STRING}, GNIL_CMD_ACCESS_DEVELOPER, true)
+GNIL.Commands.Add("antiskid-whitelist-set-check", whitelist_set_check_callback, {GNIL_CMD_ARGUMENT_STRING}, GNIL_CMD_ACCESS_DEVELOPER, true)
