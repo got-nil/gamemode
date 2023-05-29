7602c426c57b5d70c5c570ae2d31322fa2bb2e05
MorgVerd <morgverd@gmail.com>
2023-05-29 23:47:16 +0100

    refactor (net): Update emoji OOP interface

--- gamemode/modules/net/sh_emoji_oop.lua
+++ gamemode/modules/net/sh_emoji_oop.lua
@@ -1,20 +1,29 @@
 
-
 local 🚢 = GNIL.Thirdparty.middleclass("🚢", GNIL.Net.Classes.Message)
 local 🏠 = GNIL.Net.Classes.Message
 
 function 🚢:Initialize(...) 🏠.Initialize(self, ...) end
-function 🚢:✏️📐(...)  return 🏠.WriteAngle(self, ...)  end
-function 🚢:✏️🎨(...)  return 🏠.WriteColor(self, ...)  end
+function 🚢:✏️📐(...)  return 🏠.WriteAngle(self, ...) end
+function 🚢:✏️🎨(...)  return 🏠.WriteColor(self, ...) end
 function 🚢:✏️🤸(...) return 🏠.WriteEntity(self, ...) end
 function 🚢:✏️📍(...) return 🏠.WriteVector(self, ...) end
-function 🚢:✏️🔢(...) return 🏠.WriteInt(self, ...)    end
+function 🚢:✏️🔢(...) return 🏠.WriteInt(self, ...) end
 function 🚢:✏️📄(...) return 🏠.WriteString(self, ...) end
 
 function 🚢:✉️(ply) return 🏠.Send(self, ply) end
 function 🚢:📢() return 🏠.Broadcast(self) end
 function 🚢:✉️🖥️() return 🏠.SendToServer(self) end
 
+-- Reply interface.
+function 🚢:↩️(callback) return 🏠.OnReply(self, callback) end
+function 🚢:↩️⏱️(timeout) return 🏠.SetReplyTimeout(self, timeout) end
+
+-- Error interface.
+function 🚢:⚠️(callback) return 🏠.OnError(self, callback) end
+function 🚢:⚠️⏱️(callback) return 🏠.OnTimeout(self, callback) end
+function 🚢:⚠️🐌(callback) return 🏠.OnRatelimited(self, callback) end
+
+-- Global interface.
 function GNIL.Net.🛠️(...) return 🚢:New(...) end
 function 🛠️(...) return 🚢:New(...) end
 🌐 = GNIL.Net
