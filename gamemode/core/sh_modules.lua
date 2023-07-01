70ab46629815d7ac090fadb7a4c6061f7cd2b1f2
MorgVerd <morgverd@gmail.com>
2023-07-01 05:10:58 +0100

    feat (core): Detach Module Init from load

--- gamemode/core/sh_modules.lua
+++ gamemode/core/sh_modules.lua
@@ -6,21 +6,17 @@ GNIL.Modules = GNIL.Modules or {
 }
 
 -- Clear all loaded modules each time theres a LUA refresh.
-if GNIL._ENVIRONMENT == "dev" then
+if GNIL.ENV.MODULES_RESET then
     GNIL.Modules["_loaded"] = {}
 end
 
 -- A helper function for the shared gamemode file to use when loading
 -- all modules at once. It constantly checks to ensure that the module
 -- isnt loaded incase of dependency loading.
-function GNIL.Modules.LoadAll()
+function GNIL.Modules.LoadAll(_reload)
     GNIL.log("Loading all modules", "debug")
     for name, _ in pairs(GNIL.Modules.GetAll(true, true)) do
-        if GNIL.Modules.IsLoaded(name) then
-            GNIL.log("Module '" .. name .. "' is already loaded, ignoring autoload.", "debug")
-            continue
-        end
-        GNIL.Modules.Load(name)
+        GNIL.Modules.Load(name, nil, _reload)
     end
     hook.Run("GNIL.Modules.LoadedAll")
 end
@@ -47,8 +43,8 @@ function GNIL.Modules.IsLoaded(name) return GNIL.Modules._loaded[name] == true e
 -- Load a module by its name.
 --  name: The directory name of the module.
 --  _dependency_chain: Internally used to prevent dependency recursion.
-function GNIL.Modules.Load(name, _dependency_chain)
-    if GNIL.Modules.IsLoaded(name) then GNIL.log("Refusing to load module '" .. name .. "' as it is already loaded.", "debug") return false end
+function GNIL.Modules.Load(name, _dependency_chain, _reload)
+    if not _reload and GNIL.Modules.IsLoaded(name) then GNIL.log("Refusing to load module '" .. name .. "' as it is already loaded.", "debug") return false end
     if not GNIL.Modules.Exists(name) then GNIL.log("Refusing to load module '" .. name .. "' as it does not exist.", "warning") return false end
 
     -- Find the module init file to allow it to setup other things.
@@ -73,6 +69,12 @@ function GNIL.Modules.Load(name, _dependency_chain)
         ["_dependency_chain"] = _dependency_chain
     })
 
+    -- If we're reloading the module we should re-initialize it to ensure
+    -- any previous loads don't conflict (_ignored_files etc).
+    if _reload then
+        moduleInstance:Initialize(name)
+    end
+
     -- Set the MODULE const for the module to access its local instance.
     local lastModule = _G["MODULE"] or nil
     _G["MODULE"] = moduleInstance
@@ -90,16 +92,20 @@ function GNIL.Modules.Load(name, _dependency_chain)
             return false
         end
 
+        -- Call the module hook before anything.
+        if moduleInstance:OnInit() == false then
+            moduleInstance:log("Module refused to initialize.", "warning")
+        end
+
         -- Allow for other utilities etc to process the init file output.
         if hook.Run("GNIL.Modules.Init", name, moduleInstance) == false then
             moduleInstance:log("Module load was prevented by init hook.", "debug")
             return false
         end
  
-        -- Ensure that any dependencies that have not yet been resolved are resolved
-        -- once the init file has finished. Essentially delayed module dependencies.
+        -- Load all required module dependencies.
         if moduleInstance.dependencies then
-            moduleInstance:log("Resolving delayed dependencies.", "debug")
+            moduleInstance:log("Resolving dependencies.", "debug")
             for k, _ in pairs(moduleInstance.dependencies) do
                 if moduleInstance._loaded_dependencies[k] then continue end
                 moduleInstance:_ResolveRequirement(k)
@@ -111,7 +117,11 @@ function GNIL.Modules.Load(name, _dependency_chain)
     end
 
     -- Include the rest of the module directory without any of the init files.
-    moduleInstance:OnLoad() -- Call the load function/hook.
+    -- Also call OnLoad hook, allowing a final chance to reject a load.
+    if moduleInstance:OnLoad() == false or hook.Run("GNIL.Modules.Load", moduleInstance) == false then
+        moduleInstance:log("Module refused to load.", "warning")
+        return false
+    end
 
     -- Allow modules to disable autoloading. Allows init file to essentially "disable" modules.
     if moduleInstance.autoload then
