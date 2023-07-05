0131a74a093fca9fe2cd8a82d7a3526e174e6073
MorgVerd <morgverd@gmail.com>
2023-07-06 00:29:34 +0100

    fix (net): Bucket ratelimiting timing leniency.

--- gamemode/modules/net/classes/sv_bucket.lua
+++ gamemode/modules/net/classes/sv_bucket.lua
@@ -24,14 +24,15 @@ function Bucket:IsSame(capacity, refill_delay, refill_amount)
 end
 
 function Bucket:Check(steamid, requested_tokens)
+    requested_tokens = requested_tokens or 1
     if IsPlayer(steamid) then
         steamid = tostring(steamid:SteamID64())
     end
     
     -- If the player does not yet have a ratelimit, they're passing.
-    local now = RealTime()
+    local now = SysTime()
     if not self._data[steamid] then
-        self._data[steamid] = {self.capacity, now}
+        self._data[steamid] = {self.capacity - requested_tokens, now}
         return true
     end
     
@@ -40,6 +41,7 @@ function Bucket:Check(steamid, requested_tokens)
     local elapsed = now - last_refill_time
 
     -- Refill the bucket if needed.
+    if elapsed < 1 and elapsed >= 0.9 then elapsed = 1 end
     if elapsed > 0 then
         local refill = math.floor(elapsed / self.refill_delay) * self.refill_amount
         tokens = math.min(self.capacity, tokens + refill)
@@ -47,7 +49,6 @@ function Bucket:Check(steamid, requested_tokens)
     end
 
     -- Check that there are enough tokens in the bucket.
-    requested_tokens = requested_tokens or 1
     if tokens >= requested_tokens then
         self._data[steamid][1] = tokens - requested_tokens
         return true
@@ -55,10 +56,8 @@ function Bucket:Check(steamid, requested_tokens)
     return false
 end
 
--- Remove given steamid from the bucket cache.
-function Bucket:Remove(steamid)
-    self._data[steamid] = nil
-end
+function Bucket:Empty(steamid) self._data[steamid] = {0, SysTime()} end
+function Bucket:Remove(steamid) self._data[steamid] = nil end
 
 return Bucket
 
