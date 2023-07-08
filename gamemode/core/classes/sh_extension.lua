611337ed8aedcba86d510fa5f024c10cb31113ef
MorgVerd <morgverd@gmail.com>
2023-07-08 22:28:00 +0100

    feat: BaseExtension class.

---
+++ gamemode/core/classes/sh_extension.lua
@@ -0,0 +1,14 @@
+-- Module extensions allow for other modules to directly inject
+-- functionality into other modules by "Using" the extension.
+
+local BaseExtension = GNIL.Thirdparty.middleclass("BaseModuleExtension"):Include(GNIL.ClassMixins.Events)
+
+function BaseExtension:Initialize(moduleInstance)
+    self.module = moduleInstance
+end
+
+-- Module passthrough hooks.
+function BaseExtension:OnLoad(moduleInstance) end
+function BaseExtension:OnUnload(moduleInstance) end
+
+return BaseExtension
