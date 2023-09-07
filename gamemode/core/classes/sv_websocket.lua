d07c21d1256c6e5afebfa63ff45547f3698d94f8
MorgVerd <morgverd@gmail.com>
2023-09-08 00:42:46 +0100

    feat (classes): Add websocket base class

--- gamemode/core/classes/sv_websocket.lua
+++ gamemode/core/classes/sv_websocket.lua
@@ -1,2 +1,204 @@
 
-local WebsocketConnector = GNIL.Thirdparty.middleclass("WebsocketConnector")
+local Websocket = GNIL.Thirdparty.middleclass("Websocket"):IncludeMixin(GNIL.ClassMixins.Events)
+ClassAccessorFunc(Websocket, {
+    URL = {"url", FORCE_STRING},
+    VerifyCert = {"verify_cert", FORCE_BOOL},
+    RetryDelay = {
+        var = "retry_delay",
+        force = FORCE_NUMBER,
+        nillable = true,
+        validate = function(_, v)
+            return v == nil or (isnumber(v) and v > 0)
+        end
+    }
+})
+
+--[[
+
+    This is basically just a class wrapper around GWSockets.
+    With these additional features:
+        - Uses class event system so there can be multiple listeners.
+        - Seperates connection failures from normal errors.
+        - Automatically attempts connection retries.
+        - Automatically loads gwsockets dll if not already loaded.
+        - Prevent Disconnect event from being sent on initial connection failure.
+        - Prevent Open from being ran when a connection is already being opened.
+        - Provides a callback interface for Open to directly recieve connection state.
+        - More efficient IsConnected implementation.
+
+    Events:
+        message (message: str) - Message recieved
+        error (errMessage: str) - Websocket error (excluding connection failures)
+        connectionfailure - Websocket failed to connect
+        connected - Websocket connection established
+        disconnected - Websocket connection closed
+    
+--]]
+
+local function ResolveOpenCallbacks(self, ...)
+    for _, v in ipairs(self.__open_callbacks) do
+        v(...)
+    end
+    self.__opening = false
+    self.__open_callbacks = {}
+end
+
+local function GWSocketsExists()
+    
+    -- Check if the DLL has already been loaded by another module.
+    -- (If its using websockets, it really should load it itself).
+    if GNIL.Utils.IsDLLIncluded("gwsockets") then return true end
+
+    -- Attempt to require the sockets DLL.
+    local success, errorMessage = GNIL.Utils.RequireDLL("gwsockets", "GWSockets")
+    if not success then
+        GNIL.log("Could not include required websocket module with error: " .. errorMessage, "error")
+        self:EmitEvent("error", errorMessage) -- Emit error event.
+        return false
+    end
+    return true
+end
+
+local function WebsocketReconnect(self, state)
+    
+    local timerName = "gnil_ws_" .. self.__identifier
+    if state then
+
+        -- If theres no retry_delay, or we're already reconnecting ignore.
+        if self.retry_delay == nil or self.__reconnecting then return end
+        self.__reconnecting = true
+        
+        -- The websocket connection has terminated. Start a timer that
+        -- runs every retry_delay to attempt a websocket connection.
+        timer.Create(timerName, self.retry_delay, 0, function()
+            self:Open(function(state)
+
+                -- Log the state from re-connection if successful.
+                if state then
+                    GNIL.log("Successfully re-connected to websocket '" .. self.url .. "'.", "debug")
+                end
+            end)
+        end)
+    else
+        
+        -- The websocket connection has resumed. Remove the connection
+        -- retry timer to prevent it from continuing to run.
+        if not self.__reconnecting then return end
+        self.__reconnecting = false
+        if timer.Exists(timerName) then
+            timer.Remove(timerName)
+        end
+    end
+end
+
+function Websocket:Initialize(url, verify_cert, retry_delay)
+    self.url = url
+    self.verify_cert = verify_cert
+    self.retry_delay = retry_delay or 15
+
+    self.__opening = false
+    self.__open_callbacks = {}
+    self.__identifier = util.SHA256(GNIL.Utils.Random(12) .. self.url or "")
+    self.__reconnecting = false
+    self.__closed = false
+    self.__connected = false
+
+    self.__headers = {}
+    self.__cookies = {}
+end
+
+function Websocket:StopRetrying() WebsocketReconnect(self, false) return self end
+function Websocket:SetHeader(k, v) self.__headers[k] = v return self end
+function Websocket:SetCookie(k, v) self.__cookies[k] = v return self end
+
+function Websocket:Open(callback)
+
+    -- Prevent function being called when a connection
+    -- is already being opened (between Open and Connect/Disconnect)
+    if self.__opening then
+        if callback then table.insert(self.__open_callbacks, callback) end
+        return self
+    end
+
+    -- Initialize the GWSocket connection.
+    assert(callback == nil or isfunction(callback), "Provided callback argument must either be nil or a function")
+    if not GWSocketsExists() then
+        if calback then callback(false, "Failed to load GWSockets module") end
+        return self
+    end
+    local socket = GWSockets.createWebSocket(self.url, Either(isbool(self.verify_cert), self.verify_cert, false))
+
+    -- Add headers/cookies to the socket.
+    for k, v in pairs(self.__headers) do socket:setHeader(k, v) end
+    for k, v in pairs(self.__cookies) do socket:setCookie(k, v) end
+
+    -- Forward socket callbacks to class events.
+    socket.onError = function(_, errorMessage)
+        
+        -- Seperate connection failure errors from actual errors.
+        if string.sub(string.lower(errorMessage), 1, 17) == "connection failed" then
+            WebsocketReconnect(self, true)
+            return self:EmitEvent("connectionfailure", errorMessage)
+        end
+        return self:EmitEvent("error", errorMessage)
+    end
+    socket.onDisconnected = function(_, ... )
+        ResolveOpenCallbacks(self, false)
+
+        -- Ignore disconnect events when the connection isn't already open. When
+        -- the websocket fails to connect initially, it will send an error event
+        -- followed by a disconnect, despite not actually disconnecting from anything.
+        if not self.__connected then return end
+        
+        -- Set connection state and start reconnection timer if applicable.
+        self.__connected = false
+        if not self.__closed then
+            WebsocketReconnect(self, true)
+        end
+        return self:EmitEvent("disconnected", ...)
+    end
+    socket.onConnected = function(_, ...)
+        ResolveOpenCallbacks(self, true)
+
+        -- Reset reconnection state and call connected event.
+        self.__connected = true
+        WebsocketReconnect(self, false)
+        return self:EmitEvent("connected", ...)
+    end
+    socket.onMessage = function(_, ...) return self:EmitEvent("message", ...) end
+    
+    -- If there is already a socket, ensure the connection
+    -- is closed before overwriting it (losing the reference).
+    if self.__socket and self.__socket:isConnected() then
+        self.__socket:closeNow()
+    end
+
+    -- Add open callback if one exists to be called on state change.
+    if callback then table.insert(self.__open_callbacks, callback) end
+
+    -- Open the socket connection.
+    self.__closed = false
+    self.__opening = true
+    self.__socket = socket
+    self.__socket:open()
+    return self
+end
+
+function Websocket:Write(message)
+
+    -- If a table is provided, convert it to a string.
+    if istable(message) then
+        message = util.TableToJSON(message)
+    end
+    assert(isstring(message), "Provided message must be a string.")
+
+    self.__socket:write(message)
+    return self
+end
+
+function Websocket:Close() self.__closed = true return self.__socket:close() end
+function Websocket:CloseNow() self.__closed = true return self.__socket:closeNow() end
+function Websocket:ClearQueue() return self.__socket:clearQueue() end
+function Websocket:IsConnected() return self.__connected end
+
+return Websocket
