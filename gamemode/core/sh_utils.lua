345bf6478624d8ea1e212baf9c0f4b135f86d61c
MorgVerd <morgverd@gmail.com>
2023-09-21 06:36:37 +0100

    refactor (utils): Optimise IncludeDirectory

--- gamemode/core/sh_utils.lua
+++ gamemode/core/sh_utils.lua
@@ -126,31 +126,31 @@ end
 -- Load all realm prefixed files within a directory, non-recursive
 -- Absolute LUA paths are required, when working in gamemode ensure
 -- the path is locally resolved (see above)
-function GNIL.Utils.IncludeDirectory(path, ignoredFiles, absoluteIgnoredFiles)
+-- ** (last argument is deprecated, but is used in old code)
+function GNIL.Utils.IncludeDirectory(path, ignoredFiles, _)
     GNIL.log("Including directory '" .. path .. "'", "debug")
+    
+    -- If a table is provided, ensure its a lookup table.
+    if istable(ignoredFiles) then
+        if table.IsSequential(ignoredFiles) then
+            ignoredFiles = table.Lookup(ignoredFiles)
+        end
+    else
+        ignoredFiles = false
+    end
 
     for _, f in ipairs(file.Find(path .. "/*.lua", "LUA")) do
-        if istable(ignoredFiles) then
 
-            -- If there is a set of ignored files, we should check to
-            -- make sure that the file path isn't in the table. Both
-            -- relative and absolute paths are checked.
-            local seq = table.IsSequential(ignoredFiles)
-            
-            -- Allow for all ignored to be already declared as absolute,
-            -- this means we dont even have to bother checking the relative
-            -- path (which for large ignore sets would be more efficient). 
-            local ignored, absolute = false, path .. "/" .. f
-            for _, v in ipairs(absoluteIgnoredFiles == true and {absolute} or {f, absolute}) do
-                if seq then ignored = table.HasValue(ignoredFiles, v)
-                else ignored = ignoredFiles[v] == true end
-                if ignored then break end
+        -- Allow some files in the directory to be ignored.
+        if ignoredFiles then
+            local absolute = path .. "/" .. f
+            if ignoredFiles[path] == true or ignoredFiles[absolute] == true then
+                GNIL.log("Not loading filepath '" .. absolute .. "' as it is ignored.", "debug")
+                continue          
             end
+        end
 
-            -- If the file is ignored, then continue to the next one.
-            if ignored then GNIL.log("Not loading filepath '" .. absolute .. "' as it is ignored.", "debug") continue end           
-        end 
-
+        -- Make sure the file has a valid realm prefix/filename.
         local realm = GNIL.Utils.GetFilepathRealmPrefix(f)
         if realm == nil then continue end
 
