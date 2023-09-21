01658bb09e4168a5c00cd97886076cce1ff5649c
MorgVerd <morgverd@gmail.com>
2023-09-21 06:32:38 +0100

    feat (base): Update to latest

---
+++ gamemode/core/base/cl_draw.lua
@@ -0,0 +1,15 @@
+
+function draw.DrawTexturedRectRotatedOutlined(x, y, width, height, color, rotation, outlinewidth, outlinecolour)
+    local steps = ( outlinewidth * 2 ) / 3
+    if steps < 1 then steps = 1 end
+
+    surface.SetDrawColor(outlinecolour)
+    for _x = -outlinewidth, outlinewidth, steps do
+        for _y = -outlinewidth, outlinewidth, steps do
+            surface.DrawTexturedRectRotated( x + _x, y + _y, width, height, rotation )
+        end
+    end
+
+    surface.SetDrawColor(color)
+    surface.DrawTexturedRectRotated(x,y, width,height,rotation)
+end
--- gamemode/core/base/sh_table.lua
+++ gamemode/core/base/sh_table.lua
@@ -1,6 +1,6 @@
 -- Table modifications/extensions
 
-table.slice = function(tbl, first, last, step)
+table.Slice = function(tbl, first, last, step)
     local sliced = {}
   
     for i = first or 1, last or #tbl, step or 1 do
@@ -8,4 +8,12 @@ table.slice = function(tbl, first, last, step)
     end
   
     return sliced
+end
+
+table.Lookup = function(tbl, value)
+    local out, value = {}, Either(value != nil, value, true)
+    for _, v in pairs(tbl) do
+        out[v] = value 
+    end
+    return out
 end
