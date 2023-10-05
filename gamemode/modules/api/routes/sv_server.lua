4dd5481890cc55dfad1998f7d9e89d7be6ede8b2
MorgVerd <morgverd@gmail.com>
2023-10-05 22:46:56 +0100

    refactor (api): Change server response

--- gamemode/modules/api/routes/sv_server.lua
+++ gamemode/modules/api/routes/sv_server.lua
@@ -1,24 +1,29 @@
 
-GNIL.API.Routes.Get("/server", function()
+-- Base server info
+GNIL.API.Routes.Get("/server", function(request)
+
+    -- Format all enabled modules name and author.
+    local all_modules, modules = GNIL.Modules.GetAll(true), {}
+    for k, v in pairs(all_modules) do
+        if GNIL.Modules.IsLoaded(k) then
+            modules[#modules + 1] = {
+                name = v.name,
+                author = v.author   
+            }
+        end
+    end
+
     return GNIL.API.Responses.JSON({
-        map = {
-            name = game.GetMap(),
-            version = game.GetMapVersion()
-        },
-        players = {
-            current = player.GetCount(),
-            max = game.MaxPlayers()
+        success = true,
+        data = {
+            players = {
+                current = player.GetCount(),
+                max = game.MaxPlayers()
+            },
+            map = game.GetMap(),
+            ip = game.GetIPAddress(),
+            dedicated = game.IsDedicated(),
+            modules = modules
         }
     })
-end)
-
-GNIL.API.Routes.Get("/log", function(request)
-    if not request.query["msg"] then
-        return GNIL.API.Responses.Empty(400)
-    end
-    local msg = util.Base64Decode(request.query["msg"])
-    for _, v in ipairs(player.GetAll()) do
-        v:PrintMessage(HUD_PRINTTALK, msg)
-    end
-    return GNIL.API.Responses.Empty(200)
 end)
